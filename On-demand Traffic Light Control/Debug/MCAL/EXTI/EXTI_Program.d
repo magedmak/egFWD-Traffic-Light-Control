adafd38e865bb496590a3222ace70f55eea56602
Maged Magdy <96312883+magedmak@users.noreply.github.com>
2023-01-16 04:31:08 +0200

    Add files via upload

---
+++ On-demand Traffic Light Control/Debug/MCAL/EXTI/EXTI_Program.d
@@ -0,0 +1,12 @@
+MCAL/EXTI/EXTI_Program.d MCAL/EXTI/EXTI_Program.o: \
+ ../MCAL/EXTI/EXTI_Program.c ../MCAL/EXTI/EXTI_Interface.h \
+ ../MCAL/EXTI/../../utils/STD_TYPES.h ../MCAL/EXTI/../../utils/BIT_MATH.h \
+ ../MCAL/EXTI/EXTI_Private.h
+
+../MCAL/EXTI/EXTI_Interface.h:
+
+../MCAL/EXTI/../../utils/STD_TYPES.h:
+
+../MCAL/EXTI/../../utils/BIT_MATH.h:
+
+../MCAL/EXTI/EXTI_Private.h:
---
+++ On-demand Traffic Light Control/Debug/MCAL/GPIO/GPIO_Program.d
@@ -0,0 +1,12 @@
+MCAL/GPIO/GPIO_Program.d MCAL/GPIO/GPIO_Program.o: \
+ ../MCAL/GPIO/GPIO_Program.c ../MCAL/GPIO/GPIO_Interface.h \
+ ../MCAL/GPIO/../../utils/STD_TYPES.h ../MCAL/GPIO/../../utils/BIT_MATH.h \
+ ../MCAL/GPIO/GPIO_Private.h
+
+../MCAL/GPIO/GPIO_Interface.h:
+
+../MCAL/GPIO/../../utils/STD_TYPES.h:
+
+../MCAL/GPIO/../../utils/BIT_MATH.h:
+
+../MCAL/GPIO/GPIO_Private.h:
