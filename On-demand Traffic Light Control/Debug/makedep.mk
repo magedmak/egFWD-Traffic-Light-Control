0b9cf54b5d33a357e8395a4ce65e1f4193e7a34e
Maged Magdy <96312883+magedmak@users.noreply.github.com>
2023-01-17 04:41:07 +0200

    Add files via upload

---
+++ On-demand Traffic Light Control/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\APP_Program.c
+
+ECUAL\BUTTON\BUTTON_Program.c
+
+ECUAL\LED\LED_Program.c
+
+main.c
+
+MCAL\EXTI\EXTI_Program.c
+
+MCAL\GPIO\GPIO_Program.c
+
+MCAL\TMR0\TMR0_Program.c
+
+TEST\TEST_Program.c
+
